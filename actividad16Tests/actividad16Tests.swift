1e98882024c37023f665aa4fae4c9cfbb140df54
Sebasti√°n V√°squez Espinoza <Sebastian@macbook-pro-de-sebastian.local>
2015-10-12 16:31:38 -0300

    Initial Commit

---
+++ actividad16Tests/actividad16Tests.swift
@@ -0,0 +1,36 @@
+//
+//  actividad16Tests.swift
+//  actividad16Tests
+//
+//  Created by Sebastián Vásquez Espinoza on 12-10-15.
+//  Copyright © 2015 Sebastián Vásquez Espinoza. All rights reserved.
+//
+
+import XCTest
+@testable import actividad16
+
+class actividad16Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
